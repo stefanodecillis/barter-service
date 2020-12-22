5a98a5ddf37889ccc84954df5748a629f67a8e01
Sudarshan <de.sudarshan@gmail.com>
2020-12-22 09:34:38 +0530

    itemDetailScreen : Sudarshan Added ite,.id.toString() at linenum 39

--- application/lib/screen/itemDetailScreen.dart
+++ application/lib/screen/itemDetailScreen.dart
@@ -118,6 +118,7 @@ class _ItemDetailScreen extends State<ItemDetailScreen> {
     Map<String, dynamic> chatRoom = {
       "users": users,
       "chatRoomId" : chatRoomId,
+      "chatDateTime" : DateTime.now().millisecondsSinceEpoch,
     };
 
     databaseMethods.addChatRoom(chatRoom, chatRoomId);
